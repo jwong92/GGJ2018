bd9759fc081c42c5c26e26ad9c0c4921ab27826f
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-01-26 21:07:21 -0500

    updated constructors

--- GGJ-Objects.js
+++ GGJ-Objects.js
@@ -1,13 +1,23 @@
 //Planets
-var Planets() {
-  
+var Planets(posX, posY, amt, satNum, owner, img) {
+  x: posX,
+  y: posY,
+  currency: amt,
+  satellites: satNum,
+  photo: img
 }
 
-
 //Satellites
-
+var Satellites(posX, posY, range) {
+  x: posX,
+  y: posY,
+  area: range
+}
 
 //Asteroids
-
-
-//Laser Line
+var AsteroidField(posX, posY, height, width) {
+  x: posX,
+  y: posY,
+  h: height,
+  w: width
+}
