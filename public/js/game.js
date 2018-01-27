113d03303364937a476c32f00526263aea6cea69
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-01-27 04:45:45 -0500

    browser-sync added , and remote server established

--- public/js/game.js
+++ public/js/game.js
@@ -2,5 +2,5 @@
 	"use strict"
 	const canvas = document.getElementById('space-game')
 	const ctx = canvas.getContext('2d');
-	const socket = io("https://web-programmer.xyz");
+	const socket = io("https://galaxy-control.herokuapp.com");
 })()
