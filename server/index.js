f7cab3e8ca2c5835d0491b8092150171f5fb6756
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-01-27 03:39:23 -0500

    port fix2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.set('port', 3000);
+app.set('port', 80);
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 require('./routes')(app)
